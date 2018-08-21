f4740aaf50c1eb06684b51dbda6bd3f914ad062c
younghos0811 <vowhan89@naver.com>
2018-08-21 11:46:20 +0900

    [20180821]백준알고리즘-분할정복: 숫자카드2 풀이완료

--- BackJun/src/DvideNQ/NumCard2.java
+++ BackJun/src/DvideNQ/NumCard2.java
@@ -22,14 +22,35 @@ package DvideNQ;
 3 0 0 1 2 0 0 2
 */
 
-
+//tokenizer를 써보자
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class NumCard2 {
 
-    public static void main(String[] args) {
+    static final int MAX_NUM = 20000001;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = Integer.valueOf(br.readLine());
+        int[] checkCards = new int[MAX_NUM] ;
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < cnt; i++) {
+            checkCards[Integer.valueOf(tokenizer.nextToken()) + 10000000]++ ;
+        }
+
+        cnt = Integer.valueOf(br.readLine());
+        tokenizer = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < cnt; i++) {
+            sb.append(checkCards[Integer.valueOf(tokenizer.nextToken())+10000000] + " ");
+        }
+
+        System.out.println(sb.toString());
 
     }
 }
