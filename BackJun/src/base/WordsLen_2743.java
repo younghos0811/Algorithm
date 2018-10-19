eb7669abef159a62ef830c4e075c5c0ea0864640
younghos0811 <vowhan89@naver.com>
2018-10-19 17:32:33 +0900

    [20181019][백준] 단어의 길이 풀이 완료

---
+++ BackJun/src/base/WordsLen_2743.java
@@ -0,0 +1,17 @@
+package base;
+
+/**
+ *
+ *
+ * URL : https://www.acmicpc.net/problem/2743
+ *
+ */
+
+import java.util.Scanner;
+
+public class WordsLen_2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next().length());
+    }
+}
